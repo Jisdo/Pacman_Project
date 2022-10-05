02837aecb1e7aa7156959d820eb1716dcbbcd2da
Fianzz14 <setyodwiafin@gmail.com>
2022-10-05 11:52:10 +0700

    Update patch 1.1.6
    ini patch terbaru

--- Assets/Scripts/Camera.cs
+++ Assets/Scripts/Camera.cs
@@ -7,7 +7,8 @@ public class Camera : MonoBehaviour
     public Transform targetCamera;
     public float smoothSpeed;
     public Vector3 offset;
-
+    
+    //kamera om
     private void FixedUpdate()
     {
         Vector3 positionCamera = targetCamera.localPosition + offset;
