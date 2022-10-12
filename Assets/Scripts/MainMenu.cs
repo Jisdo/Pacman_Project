d25c166cefb06009b0c505c04db45e63c692a1f8
Fianzz14 <setyodwiafin@gmail.com>
2022-10-12 14:42:10 +0700

    patch 1.22
    update patch main menu baru

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-
 public class MainMenu : MonoBehaviour
 {
-    public void PlayButton()
+    public void ExitButton()
     {
+        Application.Quit();
+        Debug.Log("Pacman closed");
+
+    }
+    public void StartGame()
+    {
+        Debug.Log("Pacman closed");
         SceneManager.LoadScene("Pacman");
     }
+
 }
